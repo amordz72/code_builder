068798450c8e19f583141a4f40c11c0c88b00854
dzamor72git config --global user.name dzamor72git config --global user.name dzamor72 <dzamor72@gmail.com>
2021-12-24 06:50:54 +0100

    $this->body = "Route::get('/" . $dir . $this->name . "', App\Http\Livewire\\" . ucfirst(str_replace('/', '\\', $dir)) . ucfirst($this->name) . "/Index::class);\n";

--- app/Http/Livewire/Code/Make.php
+++ app/Http/Livewire/Code/Make.php
@@ -9,11 +9,12 @@ class Make extends Component
 {
 
     public $step = 0;
-    public $name;
+    public $name = '';
+    public $dir = '';
     public $body = '';
     protected $rules = [
 
-        'name' => 'required|min:6',
+        'name' => 'required|min:2',
     ];
     protected $messages = [
         'name.required' => 'ادخل_الاسم_اولا',
@@ -36,10 +37,24 @@ class Make extends Component
     }
     public function make_livewire_component()
     {
+        $dir = '';
+        if ($this->dir != '') {
+            $dir = $this->dir . '/';
+        }
         $this->validate();
         if ($this->step == 1) {
 
-            $this->body = "php artisan make:livewire " . $this->name;
+            $this->body = "php artisan make:livewire " . $dir . $this->name . "/index\n";
+            $this->body = "php artisan make:livewire " . $dir . $this->name . "/create\n";
+            $this->body .= "php artisan make:livewire " . $dir . $this->name . "/edit\n";
+            $this->body .= "php artisan make:livewire " . $dir . $this->name . "/show";
+
+        } else if ($this->step == 2) {
+
+            $this->body = "Route::get('/" . $dir . $this->name . "', App\Http\Livewire\\" . ucfirst(str_replace('/', '\\', $dir)) . ucfirst($this->name) . "/Index::class);\n";
+            $this->body = "Route::get('/" . $dir . $this->name . "', App\Http\Livewire\\" . ucfirst(str_replace('/', '\\', $dir)) . ucfirst($this->name) . "/Create::class);\n";
+            $this->body .= "Route::get('/" . $dir . $this->name . "', App\Http\Livewire\\" . ucfirst(str_replace('/', '\\', $dir)) . ucfirst($this->name) . "/Edit::class);\n";
+            $this->body .= "Route::get('/" . $dir . $this->name . "', App\Http\Livewire\\" . ucfirst(str_replace('/', '\\', $dir)) . ucfirst($this->name) . "/Show::class);\n";
 
         }
 
--- resources/views/livewire/code/make.blade.php
+++ resources/views/livewire/code/make.blade.php
@@ -4,11 +4,11 @@
     @endsection
     <h1 class="text-center">{{$title}}</h1>
 
-<div class="row">
+    <div class="row">
 
-    <!-- col-4 -->
-    <div class="col-md-4">
-    <div class="row mb-3">
+        <!-- col-4 -->
+        <div class="col-md-4">
+            <div class="row mb-3">
                 <select class="form-select" wire:model='step' wire:change='make_livewire_component'>
                     <option selected>select menu</option>
                     <option value="1">Create Component</option>
@@ -17,35 +17,40 @@
                 </select>
             </div>
 
-        <form wire:submit.prevent="submit">
-            <!-- steps -->
+            <form wire:submit.prevent="submit">
+                <!-- steps -->
 
 
-            <!-- name -->
-            <div class="row mb-3">
-                <input type="text" wire:model="name" class="form-control">
+                <!-- name -->
+                <div class="row mb-3">
+                 <div class="col-sm-6">
+                      <input type="text" wire:model="name" class="form-control">
+                    @error('name') <span class="error text-danger fw-bold">{{ $message }}</span> @enderror
+              </div>
+                 <div class="col-sm-6">
+                       <input type="text" wire:model="dir" class="form-control">
+                    {{-- @error('dir') <span class="error text-danger fw-bold">{{ $message }}</span> @enderror --}}
+              </div>
+             </div>
 
 
-                @error('name') <span class="error text-danger fw-bold">{{ $message }}</span> @enderror
+                <button type="submit" class="btn btn-info">Save</button>
+            </form>
+        </div>
+
+        <!-- col-8 -->
+        <div class="col-md-8">
+
+            <h3>Code</h3>
+
+            <div class="form-floating">
+                <textarea class="form-control fw-bold m-2 fs-5" placeholder="Code here" wire:model='body' style="height: 300px"></textarea>
+
             </div>
-            <button type="submit" class="btn btn-info">Save</button>
-        </form>
-    </div>
 
-    <!-- col-8 -->
-    <div class="col-md-8">
-
-       <h3>Code</h3>
-
-        <div class="form-floating">
-            <textarea class="form-control"
-             placeholder="Code here" wire:model='body' style="height: 300px"></textarea>
-            <label for="floatingTextarea2">...</label>
         </div>
 
     </div>
-
-</div>
 
 
 
